b393d21a0516c214f1bc2e1a40561a3faf8d182d
Tatiana Klanyuk <klanukt@gmail.com>
2021-04-25 18:07:14 +0300

    ссыллки

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
 function createCarousel(slidesCount = 5) {
     container = document.querySelector('#carousel');
-    
+
     slides = document.createElement('ul');
     slides.setAttribute('class', 'slides');
     container.appendChild(slides);
@@ -19,8 +19,12 @@ console.log(slides);
     for (let i = 0, n = slidesCount; i < n; i++) {
         
         const slides__item = document.createElement('li');
+        const a = document.createElement('a');
         slides__item.setAttribute('class', 'slides__item');
+        a.setAttribute('href', '#');
+        i===0 & slides__item.classList.add('active')
         slides.appendChild(slides__item);
+        slides__item.appendChild(a);
           
     }
       
